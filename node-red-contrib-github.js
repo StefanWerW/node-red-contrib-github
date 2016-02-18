d6bcd5183576bfe2d523d15216b96f6174d88463
Stefan W <scaw.dev@gmail.com>
2016-02-18 13:59:32 +0100

    Fixes

--- node-red-contrib-github.js
+++ node-red-contrib-github.js
@@ -1,25 +1,75 @@
 
 module.exports = function(RED) {
     "use strict";
-    var github-api = require('github-api');
 
     function GithubNode(n) {
         RED.nodes.createNode(this,n);
-        this.screen_name = n.screen_name;
+        this.name = n.name;
     }
     RED.nodes.registerType("github-credentials",GithubNode,{
-        credentials: {
-            screen_name: {type:"text"},
-            access_token: {type: "password"}
-        }
+      credentials: {
+        token: {type: "password"}
+      }
     });
 
     //REPO NODE
     function GithubRepo(n){
-      RED.nodes.createNode(this,config);
+      RED.nodes.createNode(this,n);
+      this.github = n.github;
+      this.username = n.username;
+      this.usernameprop = n.usernameprop;
+      this.reponame = n.repository;
+      this.repoprop = n.repoprop;
+      var github = new (require('github-api'))({
+        token: RED.nodes.getNode(n.github).credentials.token,
+        auth: "oauth"
+      });
       var node = this;
-      node.on('input', function(msg) {
+      this.on('input', function(msg) {
+        //is using user prop?
+        var username_f = "";
+        if(node.usernameprop){
+          if(msg[node.username]){
+            username_f = msg[node.username];
+          }else{
+            node.error("Property", node.username, "not fund on msg");
+            return;
+          }
+        }else{
+          if(node.username && node.username!=""){
+            username_f = node.username;
+          }else{
+            node.error("No username defined");
+            return;
+          }
+        }
 
+        var reponame_f = "";
+        if(node.repoprop){
+            if(msg[node.reponame]){
+                reponame_f = msg[node.reponame];
+            }else{
+                node.error("Property", node.reponame, "not fund on msg");
+                return;
+            }
+        }else{
+            if(node.reponame && node.reponame!=""){
+                reponame_f = node.reponame;
+            }else{
+                node.error("No repo name defined");
+                return;
+            }
+        }
+
+        var repo = github.getRepo(username_f, reponame_f);
+        repo.show(function(err, repo) {
+            if(err){
+                node.error(err);
+            }else{
+                msg.payload = repo;
+                node.send(msg);
+            }
+        });
       });
     }
     RED.nodes.registerType("github-repo", GithubRepo);
